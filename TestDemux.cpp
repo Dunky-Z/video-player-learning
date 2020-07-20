93d799fa07d922b4cb44271f95cb65b93a598fd8
如云泊 <254758318@qq.com>
2020-07-20 18:55:06 +0800

    获取视频流

--- TestDemux.cpp
+++ TestDemux.cpp
@@ -17,6 +17,12 @@ using std::endl;
 
 const char *path = "../../video/cloud.mp4";
 
+//分数运算
+static double r2d(AVRational r)
+{
+	return r.den == 0 ? 0 : (double)r.num / (double)r.den;
+}
+
 int main()
 
 {
@@ -27,7 +33,7 @@ int main()
 
 	//初始化网络库 （可以打开rtsp rtmp http 协议的流媒体视频）
 	avformat_network_init();
-	
+
 	//参数设置
 	AVDictionary *opts = NULL;
 
@@ -64,10 +70,41 @@ int main()
 	int totalMs = ic->duration / (AV_TIME_BASE / 1000);
 	cout << "totalMs = " << totalMs << endl;
 
+	//音视频索引，读取时区分音视频
+	int videoStream = 0;
+	int audioStream = 1;
+
 	//打印视频流详细信息
 	av_dump_format(ic, 0, path, 0);
 
+	for (size_t i = 0; i < ic->nb_streams; ++i)
+	{
+		AVStream *as = ic->streams[i];
+		if (as->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
+		{
+			audioStream = i;
+			cout << i << "音频信息" << endl;
+			cout << "sample_rate = " << as->codecpar->sample_rate << endl;
+			//AVSampleFormat;
+			cout << "channels = " << as->codecpar->channels << endl;
+			//一帧数据  = 单通道样本数 
+			cout << "frame_size = " << as->codecpar->frame_size << endl;
+			//1024 * 2 * 2 = 4096  fps = sample_rate/frame_size
+		}
+		else if (as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			videoStream = i;
+			cout << i << " " << "视频信息" << endl;
+			cout << "width = " << as->codecpar->width << endl;
+			cout << "height = " << as->codecpar->height << endl;
+			//帧率 fps 分数转换
+			cout << "video fps = " << r2d(as->avg_frame_rate) << endl;
+		}
+	}
 
+	//获取视频流
+	videoStream = av_find_best_stream(ic, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
+	//ic->streams[videoStream]
 
 	if (ic)
 	{
