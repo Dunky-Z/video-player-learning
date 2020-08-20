5b1b88cc43eb28cab5e68d72274ac74db7f977a8
如云泊 <254758318@qq.com>
2020-08-20 14:54:44 +0800

    解决内存泄漏问题

--- src/Xplayer/XResample.cpp
+++ src/Xplayer/XResample.cpp
@@ -65,8 +65,9 @@ int XResample::Resample(AVFrame *indata, unsigned char *d)
 		data, indata->nb_samples,		// ‰≥ˆ
 		(const uint8_t**)indata->data, indata->nb_samples	// ‰»Î
 	);
-	if (re <= 0)return re;
 	int outSize = re * indata->channels * av_get_bytes_per_sample((AVSampleFormat)outFormat);
+	av_frame_free(&indata);
+	if (re <= 0)return re;
 	return outSize;
 }
 XResample::XResample()
